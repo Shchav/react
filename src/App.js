6fee8c2376b03c3016ba6142e92ee037de89d852
Andrew Shchelykalnov <shchav@mail.ru>
2026-03-24 20:15:21 +0300

    Lesson06_Part01

--- src/App.js
+++ src/App.js
@@ -33,13 +33,36 @@ class Field extends Component {
   }
 }
 
-function WhoAmI(props) {
-  return (
-    <div>
-      <h1>My name is {props.name}, surname - {props.surname}</h1>
-      <a href={props.link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      years: 27,
+      text: '+++'
+    }
+  }
+
+  nextYear = () => {
+    console.log('+++');
+    // this.state.years++;
+    // this.setState({
+    //   years: this.state.years + 1
+    // })
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
 }
 
 function App() {
@@ -49,6 +72,7 @@ function App() {
       <Field />
       <Btn />
       <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Shepard" link="vk.com" />
     </div>
   );
 }
