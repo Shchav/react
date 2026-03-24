766c53e95cb6950b9dfb46e36081674b3cba2549
Andrew Shchelykalnov <shchav@mail.ru>
2026-03-24 20:48:19 +0300

    Lesson08

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class WhoAmI extends Component {
     super(props)
     this.state = {
       years: 27,
-      text: '+++'
+      position: ''
     }
   }
 
@@ -53,13 +53,26 @@ class WhoAmI extends Component {
     }))
   }
 
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
   render() {
     const { name, surname, link } = this.props;
+    const { position, years } = this.state;
     return (
       <div>
         <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <h1>My name is {name}, surname - {surname},
+          age - {years},
+          position - {position}</h1>
         <a href={link}>My profile</a>
+        <from>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </from>
       </div>
     )
   }
