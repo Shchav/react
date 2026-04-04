018698aeb2fb5e270a1cd32cbd0ccad54c3a1b5a
Andrew Shchelykalnov <shchav@mail.ru>
2026-04-04 14:07:43 +0300

    Lesson16

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import BootstrapTest from './BootstrapTest';
 import { Header } from './App';
+import { StrictMode } from 'react';
 
 const elem = (
   <div>
@@ -15,6 +17,9 @@ const elem = (
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // elem
-  <App />
+  <StrictMode>
+    <App />
+    <BootstrapTest />
+  </StrictMode >
 );
 
